dc77c6c4c8fcffe43bf246b31ff2c755ff40f430
Albin Varghese <albin8285@gmail.com>
2024-05-05 19:19:14 +0530

    chore: create LocationLog model

---
+++ app/Http/Controllers/LocationLogController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LocationLog;
+use App\Http\Requests\StoreLocationLogRequest;
+use App\Http\Requests\UpdateLocationLogRequest;
+
+class LocationLogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLocationLogRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LocationLog $locationLog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LocationLog $locationLog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLocationLogRequest $request, LocationLog $locationLog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LocationLog $locationLog)
+    {
+        //
+    }
+}
